9eec7a12d7550494ca61112584e9d0a5f852c993
sweetRainShin <servdb99@gmail.com>
2026-04-23 13:53:06 +0900

    chore: 프로젝트 초기 설정, 의존성 추가 및 설정 파일 작성

---
+++ src/main/java/com/firstticket/programservice/ProgramserviceApplication.java
@@ -0,0 +1,13 @@
+package com.firstticket.programservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgramserviceApplication.class, args);
+    }
+
+}
